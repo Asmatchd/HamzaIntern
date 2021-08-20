7c90161486870aba098f4f1c051a337904373cd4
Asmatchd <Asmatchd@gmail.com>
2021-08-20 15:46:29 +0500

    basics of view done

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
 import {View, Text} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View
+      style={{
+        // backgroundColor: 'black',
+        backgroundColor: '#faf',
+        // backgroundColor: 'rgba(0,0,0,0.05)',
+        height: 660,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <View
+        style={{
+          height: 400,
+          width: 410,
+          backgroundColor: 'blue',
+          alignItems: 'center',
+          // alignItems: 'flex-end',
+          justifyContent: 'center',
+          // justifyContent: 'flex-end',
+        }}>
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            backgroundColor: '#000',
+          }}></View>
+      </View>
     </View>
   );
 };
